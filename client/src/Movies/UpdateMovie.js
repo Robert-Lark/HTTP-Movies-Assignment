2cd835f683357aa023850fac0e914d1f99d40920
Rob Lark <quietcalmrecords@Robs-iMac.local>
2020-09-08 18:27:56 -0700

    completed put request

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,9 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Form from "react-basic-form";
 import { makeStyles } from "@material-ui/core/styles";
-
+import { useParams, useHistory } from "react-router-dom";
 const useStyles = makeStyles(() => ({
 	updateForm: {
 		display: "flex",
@@ -13,99 +14,93 @@ const useStyles = makeStyles(() => ({
 	},
 }));
 const initialState = {
-    id: "",
-    title: "",
-    director: "",
-    metascore: "",
-    stars: []
-}
-
-function UpdateMovie(props) {
-
-const [update, setUpdate] = useState(initialState)
-
-const classes = useStyles();
-
-const submit = (e) => {
-    e.preventDefault();
-   
-    console.log(update)
-	// axios
-	// 	.post("http://localhost:5000/api/login", formValues)
-	// 	.then((res) => {
-	// 		localStorage.setItem("token", res.data.payload);
-	// 		props.history.push("/protected");
-	// 	})
-	// 	.catch((err) => console.log("error: ", err));
+	id: "",
+	title: "",
+	director: "",
+	metascore: "",
+	stars: [],
 };
 
-const changeHandler = (e) => {
-     setUpdate({ ...update, [e.target.name]: e.target.value })
-    }
+function UpdateMovie(props) {
+	const [update, setUpdate] = useState(initialState);
+	const { push } = useHistory();
+	const { id } = useParams();
+	useEffect(() => {
+		axios
+			.get(`http://localhost:5000/api/movies/${id}`)
+			.then((res) => {
+				setUpdate(res.data);
+			})
+			.catch((err) => console.log(err));
+	}, [id]);
 
-    return (
-			<Grid container className={classes.updateForm}>
-				<Form onSubmit={(data) => console.log(data)}>
-					<Form.Element
-						onChange={changeHandler}
-						label="id"
-						name="id"
-						required
-					/>
-					<Form.Element
-						onChange={changeHandler}
-						label="title"
-						name="title"
-						type="email"
-						required
-					/>
-					<Form.Element
-						onChange={changeHandler}
-						label="director"
-						name="director"
-						required
-					/>
-					<Form.Element
-						label="metascore"
-						name="metascore"
-						type="email"
-						onChange={changeHandler}
-						required
-					/>
-					<h4>ACTORS</h4>
-					<Form.Element
-						onChange={changeHandler}
-						label="stars"
-						name="stars"
-						type="text"
-						required
-					/>
-					<Form.Element
-						onChange={changeHandler}
-						label="stars"
-						name="stars"
-						type="text"
-						required
-					/>
-					<Form.Element
-						label="stars"
-						name="stars"
-						type="text"
-						onChange={changeHandler}
-						required
-					/>
-				</Form>
-				<Button
-					onClick={submit}
-					value="value"
-					variant="contained"
-				>
-					UPDATE MOVIE
-				</Button>
-			</Grid>
-		);
+	const classes = useStyles();
 
+	const submit = (e) => {
+		e.preventDefault();
 
+		console.log(update);
+		axios
+			.put(`http://localhost:5000/api/movies/${id}`, update)
+			.then((res) => {
+				setUpdate(res.data)
+				push("/movies")
+			})
+			.catch((err) => console.log("error: ", err));
+	};
+
+	const changeHandler = (e) => {
+		setUpdate({ ...update, [e.target.name]: e.target.value });
+	};
+
+	return (
+		<Grid container className={classes.updateForm}>
+			<Form onSubmit={(data) => console.log(data)}>
+				<Form.Element
+					onChange={changeHandler}
+					label="id"
+					name="id"
+					value={update.id}
+					required
+				/>
+				<Form.Element
+					onChange={changeHandler}
+					label="title"
+					name="title"
+					type="email"
+					value={update.title}
+					required
+				/>
+				<Form.Element
+					onChange={changeHandler}
+					label="director"
+					value={update.director}
+					name="director"
+					required
+				/>
+				<Form.Element
+					label="metascore"
+					name="metascore"
+					value={update.metascore}
+					type="email"
+					onChange={changeHandler}
+					required
+				/>
+				<h4>ACTORS</h4>
+				<Form.Element
+					onChange={changeHandler}
+					label="stars"
+					value={update.stars}
+					name="stars"
+					type="text"
+					required
+				/>
+			</Form>
+			<Button onClick={submit} value="value" variant="contained">
+				UPDATE MOVIE
+			</Button>
+		</Grid>
+	);
 }
 
 export default UpdateMovie;
